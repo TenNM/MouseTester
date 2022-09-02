e5c1b56789d971c12977f3fff3575904aa44331b
TenNM <pacs3131@gmail.com>
2022-09-02 16:31:39 +0300

    - form and exe icons added

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.textBoxClickPos = new System.Windows.Forms.TextBox();
             this.panelL = new System.Windows.Forms.Panel();
             this.numericUpDownL = new System.Windows.Forms.NumericUpDown();
@@ -324,6 +325,7 @@
             this.Controls.Add(this.panelL);
             this.Controls.Add(this.textBoxClickPos);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
